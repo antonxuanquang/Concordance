e2924f762a1c063b45b7b4ca0bf0202bb3a9e9bb
antonxuanquang <antonxuanquang@yahoo.com>
2015-10-21 00:57:25 -0500

    adding interface

---
+++ src/Interface/ContextNodeInterface.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public interface ContextNodeInterface {
+	public int getParagraphNum();
+	public void setParagraphNum(int i);
+	public int getSentenceNum();
+	public void setSentenceNum(int i);
+	public String getContext();
+	public void setContext(String s);
+	public ContextNode getNext();
+	public void setNext(ContextNode n);
+}
